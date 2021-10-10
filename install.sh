19bcf0fa50adf8ecd43723e5f607b1195a0ca194
Cledson Ferreira <cledsonitgames@gmail.com>
2021-10-10 00:00:11 -0300

    Serviço implementado com sucesso; adicionada a GPLv3

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/data/data/com.termux/files/usr/bin/sh
+
+#    This file is part of BatService.
+#
+#    BatService is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    BatService is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with BatService.  If not, see <https://www.gnu.org/licenses/>.
+
+if [ $(id -u) -eq 0 ]; then
+  echo "O BatService não pode ser instalado no Termux em modo root!"
+  exit 1
+fi
+
+install_file() {
+  if [ ! -d "$2" ]; then
+    mkdir -p "$2"
+    e=$?
+    if [ $e -ne 0 ]; then
+      echo "Não foi possível criar o diretório \"$2\"!"
+      exit $e
+    fi
+  fi
+
+  echo "Copiando \"$1\" em \"$2\"..."
+  cp "$1" "$2"
+  e=$?
+  if [ $e -ne 0 ]; then
+    echo "Falha ao copiar!"
+    exit $e
+  fi
+}
+
+echo "Instalando BatService..."
+install_file batservice.sh $PREFIX/bin/
+chmod +x $PREFIX/bin/batservice.sh
+
+install_file lib/startup-helper.sh $PREFIX/lib/batservice/
+chmod +x $PREFIX/lib/batservice/startup-helper.sh
+
+install_file tools/batservice-boot.sh $HOME/.termux/boot/
+chmod +x $HOME/.termux/boot/batservice-boot.sh
+
+echo "Instalação concluída!"
+echo "by cleds.upper"
---
+++ test/README.txt
@@ -0,0 +1,6 @@
+Estes arquivos são para testar o programa com as seguintes variáveis de ambiente definidas:
+  BWD="*/test/tBWD" (ou qualquer outro endereço simulando o sysfs da bateria)
+  EXIT_FILE="*/test/batservice.exit" (ou qualquer outro arquivo para saída de erros)
+  NO_SERVICE=1 (dispensa root em testes)
+  
+  by cleds.upper
