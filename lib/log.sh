0a3ede806eeb39f2064d69a66e2060119d43a0c4
Cledson Ferreira <cledsonitgames@gmail.com>
2021-12-09 14:41:16 -0300

    Copyright notes

--- lib/log.sh
+++ lib/log.sh
@@ -1,3 +1,20 @@
+#    log.sh - redirect output to a file.
+#
+#    This file is part of BatService.
+#
+#    BatService is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    BatService is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with BatService.  If not, see <https://www.gnu.org/licenses/>.
+
 CACHE_FILE="$CACHE/out.log"
 
 _logging=0
--- module-create.sh
+++ module-create.sh
@@ -1,6 +1,22 @@
 #!/system/bin/sh
 
-# Este script apenas empacota o módulo com todos os arquivos necessários
+#    module-create.sh - this script generates a BatService module package
+#    for Magisk.
+#
+#    This file is part of BatService.
+#
+#    BatService is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    BatService is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with BatService.  If not, see <https://www.gnu.org/licenses/>.
 
 NAME=BatService-A20
 VERSION=v2.1.211209
