eec4652a5f012ab304be32ed4532ccdea478bc0e
Cledson Ferreira <cledsonitgames@gmail.com>
2021-12-09 15:12:12 -0300

    Faltando log.sh

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ chmod +x $PREFIX/lib/batservice/notify.sh
 install_file lib/env.rc $PREFIX/lib/batservice/
 install_file lib/jobs.sh $PREFIX/lib/batservice/
 install_file lib/consts.sh $PREFIX/lib/batservice/
+install_file lib/log.sh $PREFIX/lib/batservice/
 
 boot_file=$(ls $HOME/.termux/boot/batservice-*.sh 2>/dev/null)
 expected_boot_file="$HOME/.termux/boot/batservice-termux.sh"
