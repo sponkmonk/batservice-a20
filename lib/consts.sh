0bf528081392521f3a2c092f45c81a39188cda5b
Cledson Ferreira <cledsonitgames@gmail.com>
2021-12-15 00:46:55 -0300

    v2.1.211215

--- lib/consts.sh
+++ lib/consts.sh
@@ -14,7 +14,7 @@
 #    along with BatService.  If not, see <https://www.gnu.org/licenses/>.
 
 NAME="BatService"
-VERSION="2.1.211211"
+VERSION="2.1.211215"
 
 ENABLED=1
 DISABLED=0
--- module-create.sh
+++ module-create.sh
@@ -19,7 +19,7 @@
 #    along with BatService.  If not, see <https://www.gnu.org/licenses/>.
 
 NAME=BatService-A20
-VERSION=v2.1.211212
+VERSION=v2.1.211215
 
 echo "Criando pacote Magisk..."
 
